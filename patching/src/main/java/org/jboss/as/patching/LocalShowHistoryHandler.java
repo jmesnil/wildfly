emuckenhuber/jboss-as

[AS7-462] Patching system
* add show-history operation
* store the timestamp at which a patch has been applied

patching/src/main/java/org/jboss/as/patching/LocalShowHistoryHandler.java
@@ -1,0 +1,86 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2012, Red Hat, Inc., and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+
+package org.jboss.as.patching;
+
+import static org.jboss.as.patching.PatchInfo.BASE;
+import static org.jboss.as.patching.metadata.Patch.PatchType.CUMULATIVE;
+import static org.jboss.as.patching.metadata.Patch.PatchType.ONE_OFF;
+
+import java.io.File;
+import java.util.List;
+
+import org.jboss.as.boot.DirectoryStructure;
+import org.jboss.as.controller.OperationContext;
+import org.jboss.as.controller.OperationFailedException;
+import org.jboss.as.controller.OperationStepHandler;
+import org.jboss.as.patching.metadata.Patch.PatchType;
+import org.jboss.as.patching.runner.PatchUtils;
+import org.jboss.dmr.ModelNode;
+
+/**
+ * @author <a href="http://jmesnil.net/">Jeff Mesnil</a> (c) 2012, Red Hat Inc
+ */
+public final class LocalShowHistoryHandler implements OperationStepHandler {
+    public static final OperationStepHandler INSTANCE = new LocalShowHistoryHandler();
+
+    @Override
+    public void execute(final OperationContext context, final ModelNode operation) throws OperationFailedException {
+        context.acquireControllerLock();
+        // Setup
+        final PatchInfoService service = (PatchInfoService) context.getServiceRegistry(false).getRequiredService(PatchInfoService.NAME).getValue();
+
+        try {
+            final PatchInfo info = service.getPatchInfo();
+            final DirectoryStructure structure = service.getStructure();
+            ModelNode result = new ModelNode();
+            result.setEmptyList();
+
+            String cumulativePatchID = info.getCumulativeID();
+            if (!BASE.equals(cumulativePatchID)) {
+                fillHistory(result, CUMULATIVE, cumulativePatchID, structure.getHistoryDir(cumulativePatchID));
+            }
+
+            List<String> oneOffPatchIDs = info.getPatchIDs();
+            for (String oneOffPatchID : oneOffPatchIDs) {
+                File historyDir = structure.getHistoryDir(oneOffPatchID);
+                fillHistory(result, ONE_OFF, oneOffPatchID, historyDir);
+            }
+            context.getResult().set(result);
+            context.stepCompleted();
+        } catch (Throwable t) {
+            t.printStackTrace();
+            throw PatchMessages.MESSAGES.failedToShowHistory(t);
+        }
+    }
+
+    private void fillHistory(ModelNode result, PatchType type, String oneOffPatchID, File historyDir) throws Exception {
+        ModelNode history = new ModelNode();
+        history.get(type.getName()).set(oneOffPatchID);
+
+        File timestampFile = new File(historyDir, Constants.TIMESTAMP);
+        String timestamp = PatchUtils.readRef(timestampFile);
+        history.get(Constants.APPLIED_AT).set(timestamp);
+        result.add(history);
+    }
+
+}

patching/src/main/java/org/jboss/as/patching/runner/PatchingContext.java
@@ -22,7 +22,10 @@
 
 package org.jboss.as.patching.runner;
 
+import static org.jboss.as.patching.runner.PatchUtils.generateTimestamp;
+
 import org.jboss.as.boot.DirectoryStructure;
+import org.jboss.as.patching.Constants;
 import org.jboss.as.patching.LocalPatchInfo;
 import org.jboss.as.patching.PatchInfo;
 import org.jboss.as.patching.PatchLogger;
@@ -233,9 +236,11 @@ class PatchingContext {
         // Backup the current active patch Info
         final File cumulativeBackup = new File(backup, DirectoryStructure.CUMULATIVE);
         final File referencesBackup = new File(backup, DirectoryStructure.REFERENCES);
+        final File timestamp = new File(backup, Constants.TIMESTAMP);
         try {
             PatchUtils.writeRef(cumulativeBackup, info.getCumulativeID());
             PatchUtils.writeRefs(referencesBackup, info.getPatchIDs());
+            PatchUtils.writeRef(timestamp, generateTimestamp());
         } catch (IOException e) {
             throw  new PatchingException(e);
         }

jmesnil/wildfly

patching/src/main/java/org/jboss/as/patching/LocalShowHistoryHandler.java
@@ -1,0 +1,86 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2012, Red Hat, Inc., and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+
+package org.jboss.as.patching;
+
+import static org.jboss.as.patching.PatchInfo.BASE;
+import static org.jboss.as.patching.metadata.Patch.PatchType.CUMULATIVE;
+import static org.jboss.as.patching.metadata.Patch.PatchType.ONE_OFF;
+
+import java.io.File;
+import java.util.List;
+
+import org.jboss.as.boot.DirectoryStructure;
+import org.jboss.as.controller.OperationContext;
+import org.jboss.as.controller.OperationFailedException;
+import org.jboss.as.controller.OperationStepHandler;
+import org.jboss.as.patching.metadata.Patch.PatchType;
+import org.jboss.as.patching.runner.PatchUtils;
+import org.jboss.dmr.ModelNode;
+
+/**
+ * @author <a href="http://jmesnil.net/">Jeff Mesnil</a> (c) 2012, Red Hat Inc
+ */
+public final class LocalShowHistoryHandler implements OperationStepHandler {
+    public static final OperationStepHandler INSTANCE = new LocalShowHistoryHandler();
+
+    @Override
+    public void execute(final OperationContext context, final ModelNode operation) throws OperationFailedException {
+        context.acquireControllerLock();
+        // Setup
+        final PatchInfoService service = (PatchInfoService) context.getServiceRegistry(false).getRequiredService(PatchInfoService.NAME).getValue();
+
+        try {
+            final PatchInfo info = service.getPatchInfo();
+            final DirectoryStructure structure = service.getStructure();
+            ModelNode result = new ModelNode();
+            result.setEmptyList();
+
+            String cumulativePatchID = info.getCumulativeID();
+            if (!BASE.equals(cumulativePatchID)) {
+                fillHistory(result, CUMULATIVE, cumulativePatchID, structure.getHistoryDir(cumulativePatchID));
+            }
+
+            List<String> oneOffPatchIDs = info.getPatchIDs();
+            for (String oneOffPatchID : oneOffPatchIDs) {
+                File historyDir = structure.getHistoryDir(oneOffPatchID);
+                fillHistory(result, ONE_OFF, oneOffPatchID, historyDir);
+            }
+            context.getResult().set(result);
+            context.stepCompleted();
+        } catch (Throwable t) {
+            t.printStackTrace();
+            throw PatchMessages.MESSAGES.failedToShowHistory(t);
+        }
+    }
+
+    private void fillHistory(ModelNode result, PatchType type, String oneOffPatchID, File historyDir) throws Exception {
+        ModelNode history = new ModelNode();
+        history.get(type.getName()).set(oneOffPatchID);
+
+        File timestampFile = new File(historyDir, CommonAttributes.TIMESTAMP);
+        String timestamp = PatchUtils.readRef(timestampFile);
+        history.get(CommonAttributes.APPLIED_AT).set(timestamp);
+        result.add(history);
+    }
+
+}

patching/src/main/java/org/jboss/as/patching/runner/PatchingContext.java
@@ -22,7 +22,10 @@
 
 package org.jboss.as.patching.runner;
 
+import static org.jboss.as.patching.runner.PatchUtils.generateTimestamp;
+
 import org.jboss.as.boot.DirectoryStructure;
+import org.jboss.as.patching.CommonAttributes;
 import org.jboss.as.patching.LocalPatchInfo;
 import org.jboss.as.patching.PatchInfo;
 import org.jboss.as.patching.PatchLogger;
@@ -233,9 +236,11 @@ class PatchingContext {
         // Backup the current active patch Info
         final File cumulativeBackup = new File(backup, DirectoryStructure.CUMULATIVE);
         final File referencesBackup = new File(backup, DirectoryStructure.REFERENCES);
+        final File timestamp = new File(backup, CommonAttributes.TIMESTAMP);
         try {
             PatchUtils.writeRef(cumulativeBackup, info.getCumulativeID());
             PatchUtils.writeRefs(referencesBackup, info.getPatchIDs());
+            PatchUtils.writeRef(timestamp, generateTimestamp());
         } catch (IOException e) {
             throw  new PatchingException(e);
         }