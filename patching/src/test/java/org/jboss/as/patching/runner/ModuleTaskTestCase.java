a4a5133927f7595ad53be77b67817aefa705ef06
Jeff Mesnil <jmesnil@gmail.com>
2012-10-22 16:57:54 +0200

    add test to rollback a CP

--- patching/src/test/java/org/jboss/as/patching/runner/ModuleTaskTestCase.java
+++ patching/src/test/java/org/jboss/as/patching/runner/ModuleTaskTestCase.java
@@ -87,6 +87,7 @@ public class ModuleTaskTestCase extends AbstractTaskTestCase{
 
         File modulesPatchDir = env.getModulePatchDirectory(patchID);
         assertDirExists(modulesPatchDir);
+        tree(result.getPatchInfo().getEnvironment().getInstalledImage().getJbossHome());
         assertContains(modulesPatchDir, result.getPatchInfo().getModulePath());
         assertDefinedModule(result.getPatchInfo().getModulePath(), moduleName, newHash);
     }
