0eed3cf6d5d292858f228fbc66ef4f905f18fb38
Jeff Mesnil <jmesnil@gmail.com>
2012-10-22 16:57:54 +0200

    add test to rollback a CP

--- patching/src/test/java/org/jboss/as/patching/runner/CumulativePatchTestCase.java
+++ patching/src/test/java/org/jboss/as/patching/runner/CumulativePatchTestCase.java
@@ -24,16 +24,22 @@ package org.jboss.as.patching.runner;
 
 import static org.jboss.as.patching.metadata.ModificationType.ADD;
 import static org.jboss.as.patching.metadata.ModificationType.MODIFY;
-import static org.jboss.as.patching.runner.PatchUtils.bytesToHexString;
 import static org.jboss.as.patching.runner.PatchUtils.calculateHash;
 import static org.jboss.as.patching.runner.PatchingAssert.assertDefinedModule;
+import static org.jboss.as.patching.runner.PatchingAssert.assertDirExists;
+import static org.jboss.as.patching.runner.PatchingAssert.assertFileExists;
 import static org.jboss.as.patching.runner.PatchingAssert.assertPatchHasBeenApplied;
+import static org.jboss.as.patching.runner.PatchingAssert.assertPatchHasBeenRolledBack;
 import static org.jboss.as.patching.runner.PatchingTask.NO_CONTENT;
 import static org.jboss.as.patching.runner.TestUtils.createModule;
 import static org.jboss.as.patching.runner.TestUtils.createPatchXMLFile;
 import static org.jboss.as.patching.runner.TestUtils.createZippedPatchFile;
+import static org.jboss.as.patching.runner.TestUtils.dump;
 import static org.jboss.as.patching.runner.TestUtils.mkdir;
 import static org.jboss.as.patching.runner.TestUtils.randomString;
+import static org.jboss.as.patching.runner.TestUtils.touch;
+import static org.jboss.as.patching.runner.TestUtils.tree;
+import static org.junit.Assert.assertArrayEquals;
 
 import java.io.File;
 import java.io.FileInputStream;
@@ -42,6 +48,7 @@ import java.util.Collections;
 import org.jboss.as.patching.LocalPatchInfo;
 import org.jboss.as.patching.PatchInfo;
 import org.jboss.as.patching.metadata.ContentModification;
+import org.jboss.as.patching.metadata.MiscContentItem;
 import org.jboss.as.patching.metadata.ModuleItem;
 import org.jboss.as.patching.metadata.Patch;
 import org.jboss.as.patching.metadata.Patch.PatchType;
@@ -66,7 +73,7 @@ public class CumulativePatchTestCase extends AbstractTaskTestCase{
         String moduleName = randomString();
         File moduleDir = createModule(patchDir, moduleName);
         byte[] newHash = calculateHash(moduleDir);
-        ContentModification moduleAdded = new ContentModification(new ModuleItem(moduleName, NO_CONTENT), newHash, ADD);
+        ContentModification moduleAdded = new ContentModification(new ModuleItem(moduleName, newHash), NO_CONTENT , ADD);
 
         Patch patch = PatchBuilder.create()
                 .setPatchId(patchID)
@@ -83,8 +90,66 @@ public class CumulativePatchTestCase extends AbstractTaskTestCase{
         PatchingResult result = runner.executeDirect(new FileInputStream(zippedPatch), ContentVerificationPolicy.STRICT);
 
         assertPatchHasBeenApplied(result, patch);
-
+        tree(result.getPatchInfo().getEnvironment().getInstalledImage().getJbossHome());
         assertDefinedModule(result.getPatchInfo().getModulePath(), moduleName, newHash);
+    }
+
+    @Test
+    public void testApplyCumulativePatchAndRollback() throws Exception {
+
+        // start from a base installation
+        PatchInfo info = new LocalPatchInfo(randomString(), PatchInfo.BASE, Collections.<String>emptyList(), env);
+        // create an existing file in the AS7 installation
+        File binDir = mkdir(env.getInstalledImage().getJbossHome(), "bin");
+        String fileName = "standalone.sh";
+        File standaloneShellFile = touch(binDir, fileName);
+        dump(standaloneShellFile, "original script to run standalone AS7");
+        byte[] existingHash = calculateHash(standaloneShellFile);
+
+        // build a CP patch for the base installation
+        // with 1 added module
+        // and 1 updated file
+        String patchID = randomString();
+        File patchDir = mkdir(tempDir, patchID);
+        String moduleName = randomString();
+        File moduleDir = createModule(patchDir, moduleName);
+        byte[] newModuleHash = calculateHash(moduleDir);
+        ContentModification moduleAdded = new ContentModification(new ModuleItem(moduleName, newModuleHash), NO_CONTENT, ADD);
+        File updatedFile = touch(patchDir, "misc", "bin", fileName);
+        dump(updatedFile, "updated script");
+        byte[] updatedHash = calculateHash(updatedFile);
+        ContentModification fileUpdated = new ContentModification(new MiscContentItem(fileName, new String[] { "bin" }, updatedHash), existingHash, MODIFY);
+
+        Patch patch = PatchBuilder.create()
+                .setPatchId(patchID)
+                .setDescription(randomString())
+                .setPatchType(PatchType.CUMULATIVE)
+                .setResultingVersion(info.getVersion() + "-CP")
+                .addAppliesTo(info.getVersion())
+                .addContentModification(moduleAdded)
+                .addContentModification(fileUpdated)
+                .build();
+        createPatchXMLFile(patchDir, patch);
+        File zippedPatch = createZippedPatchFile(patchDir, patchID);
+
+        PatchingTaskRunner runner = new PatchingTaskRunner(info, env);
+        PatchingResult result = runner.executeDirect(new FileInputStream(zippedPatch), ContentVerificationPolicy.STRICT);
+
+        assertPatchHasBeenApplied(result, patch);
+        assertFileExists(standaloneShellFile);
+        assertArrayEquals(updatedHash, calculateHash(standaloneShellFile));
+        tree(result.getPatchInfo().getEnvironment().getInstalledImage().getJbossHome());
+        assertDirExists(result.getPatchInfo().getEnvironment().getPatchDirectory(patchID));
+        assertDefinedModule(result.getPatchInfo().getModulePath(), moduleName, newModuleHash);
+
+        // rollback the patch based on the updated PatchInfo
+        runner = new PatchingTaskRunner(result.getPatchInfo(), result.getPatchInfo().getEnvironment());
+        PatchingResult rollbackResult = runner.rollback(patchID, true);
+
+        tree(result.getPatchInfo().getEnvironment().getInstalledImage().getJbossHome());
+        assertPatchHasBeenRolledBack(rollbackResult, patch, info);
+        assertFileExists(standaloneShellFile);
+        assertArrayEquals(existingHash, calculateHash(standaloneShellFile));
     }
 
     @Test
@@ -140,7 +205,7 @@ public class CumulativePatchTestCase extends AbstractTaskTestCase{
         File zippedOneOffPatch = createZippedPatchFile(oneOffPatchDir, oneOffPatchID);
 
         // use the updated PatchInfo for the result of applying the cumulative patch
-        runner = new PatchingTaskRunner(resultOfCumulativePatch.getPatchInfo(), env);
+        runner = new PatchingTaskRunner(resultOfCumulativePatch.getPatchInfo(), resultOfCumulativePatch.getPatchInfo().getEnvironment());
         PatchingResult resultOfOneOffPatch = runner.executeDirect(new FileInputStream(zippedOneOffPatch), ContentVerificationPolicy.STRICT);
 
         assertPatchHasBeenApplied(resultOfOneOffPatch, oneOffPatch);
