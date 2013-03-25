f819eaea165163a443baf35ec11aa090002b4627
Jeff Mesnil <jmesnil@gmail.com>
2013-03-25 17:45:47 +0100

    [AS7-370] Notification Support
    Notification emitted by OperationContext.emit(Notification) are
    effectively sent at the end of the operation execution if it is
    successful. This ensure that the order of notifications emitted by a
    single OperationContext will be received in the same order (unless
    they are emitted during the ResultHandler execution).

--- controller/src/main/java/org/jboss/as/controller/AbstractAddStepHandler.java
+++ controller/src/main/java/org/jboss/as/controller/AbstractAddStepHandler.java
@@ -22,15 +22,10 @@
 
 package org.jboss.as.controller;
 
-import static org.jboss.as.controller.ControllerMessages.MESSAGES;
-import static org.jboss.as.controller.PathAddress.pathAddress;
-import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.OP_ADDR;
-import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.RESOURCE_ADDED_NOTIFICATION;
-
 import java.util.ArrayList;
 import java.util.List;
 
-import org.jboss.as.controller.notification.NotificationResultHandler;
+import org.jboss.as.controller.notification.NotificationUtil;
 import org.jboss.as.controller.registry.Resource;
 import org.jboss.dmr.ModelNode;
 import org.jboss.msc.service.ServiceController;
@@ -69,8 +64,8 @@ public abstract class AbstractAddStepHandler implements OperationStepHandler {
             }, OperationContext.Stage.RUNTIME);
         }
 
-        final PathAddress address = pathAddress(operation.require(OP_ADDR));
-        context.completeStep(new NotificationResultHandler(RESOURCE_ADDED_NOTIFICATION, MESSAGES.resourceWasAdded(address)));
+        NotificationUtil.emitResourceAdded(context, operation);
+        context.stepCompleted();
     }
 
     /**
