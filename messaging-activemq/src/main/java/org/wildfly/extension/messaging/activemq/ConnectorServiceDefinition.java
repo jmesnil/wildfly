8304d33f9df91a1905231d769f94e93d24a2530c
Jeff Mesnil <jmesnil@gmail.com>
2015-02-26 14:11:20 +0100

    more persistent XML resource

--- messaging-activemq/src/main/java/org/wildfly/extension/messaging/activemq/ConnectorServiceDefinition.java
+++ messaging-activemq/src/main/java/org/wildfly/extension/messaging/activemq/ConnectorServiceDefinition.java
@@ -39,7 +39,6 @@ import org.jboss.as.controller.OperationStepHandler;
 import org.jboss.as.controller.PathElement;
 import org.jboss.as.controller.PersistentResourceDefinition;
 import org.jboss.as.controller.ReloadRequiredWriteAttributeHandler;
-import org.jboss.as.controller.SimpleResourceDefinition;
 import org.jboss.as.controller.registry.ManagementResourceRegistration;
 import org.jboss.dmr.ModelNode;
 import org.jboss.dmr.Property;
--- messaging-activemq/src/main/java/org/wildfly/extension/messaging/activemq/ConnectorServiceParamDefinition.java
+++ messaging-activemq/src/main/java/org/wildfly/extension/messaging/activemq/ConnectorServiceParamDefinition.java
@@ -32,7 +32,6 @@ import org.jboss.as.controller.OperationStepHandler;
 import org.jboss.as.controller.PathElement;
 import org.jboss.as.controller.PersistentResourceDefinition;
 import org.jboss.as.controller.SimpleAttributeDefinition;
-import org.jboss.as.controller.SimpleResourceDefinition;
 import org.jboss.as.controller.registry.ManagementResourceRegistration;
 import org.jboss.dmr.ModelType;
 
