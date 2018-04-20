cd522818fcfcad5cb669881303074c69df3a3270
Jeff Mesnil <jmesnil@gmail.com>
2018-04-20 18:45:32 +0200

    [WIP] Support Eclipse MicroProfile Config

---
+++ microprofile/config/src/main/java/org/wildfly/extension/microprofile/config/SubsystemRemove.java
@@ -0,0 +1,27 @@
+package org.wildfly.extension.microprofile.config;
+
+import org.jboss.as.controller.AbstractRemoveStepHandler;
+import org.jboss.as.controller.OperationContext;
+import org.jboss.as.controller.OperationFailedException;
+import org.jboss.dmr.ModelNode;
+
+/**
+ * Handler responsible for removing the subsystem resource from the model
+ *
+ * @author <a href="kabir.khan@jboss.com">Kabir Khan</a>
+ */
+class SubsystemRemove extends AbstractRemoveStepHandler {
+
+    static final SubsystemRemove INSTANCE = new SubsystemRemove();
+
+
+    private SubsystemRemove() {
+    }
+
+    @Override
+    protected void performRuntime(OperationContext context, ModelNode operation, ModelNode model) throws OperationFailedException {
+        context.removeService(ServiceNames.CONFIG_PROVIDER);
+    }
+
+
+}
